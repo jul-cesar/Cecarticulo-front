04b39f0665c5b4c311e96f55bb4d44a845ec78be
Julio Cesar Martinez <juliocesarmartinez1344@gmail.com>
2025-08-30 14:10:56 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="bg-blue-800">
+      <h1 className="text-white text-3xl font-bold underline">Hello world!</h1>
+      <Button>AKDJKASD</Button>
+    </div>
+  );
+}
+
+export default App;
